418b4617d5bef07230e625d3f23622a34189f5c4
Daniele <daniele.devito@live.ca>
2017-04-25 19:39:44 -0400

    Search page update? part of batch ftp updating

--- wp-content/themes/ddevito-starter-theme/lib/Pages/SearchPage.php
+++ wp-content/themes/ddevito-starter-theme/lib/Pages/SearchPage.php
@@ -14,21 +14,32 @@
 
 namespace TBSC\Pages;
 
-use TBSC\Posts\Archive;
+use TBSC_Core\Navigation\Pagination;
 use TBSC_Core\Support\Views;
+use TBSC_Core\Config\ArrayConfig;
+use TBSC_Core\Support\CustomData;
 
-class SearchPage extends Archive {
+class SearchPage {
 
-	/**
-	 * Output the archive page header.
-	 *
-	 * For archives this will include the page title
-	 * as well as the archive description.
-	 *
-	 * @since 2.0.0
-	 */
+	public function __construct() {
+
+		$this->pagination = new Pagination(
+			new ArrayConfig( TBSC_CONFIG_DIR . 'navigation/paginate-links-search.php' )
+		);
+		$this->common_meta = CustomData::get_the_post_meta( '_tbsc_common_custom_post_meta', false )[0];
+	}
+
+	public function do_the_entry() {
+		add_action( 'ezra_before_entry', array( $this, 'do_page_header' ) );
+		add_action( 'ezra_entry', array( $this, 'do_page_content' ) );
+		add_action( 'ezra_after_loop', array( $this->pagination, 'do_pagination' ) );
+	}
+
 	public function do_page_header() {
 		include( Views::load_view( TBSC_VIEWS_DIR . 'search/search-page-header.php' ) );
 	}
 
+	public function do_page_content() {
+		include( Views::load_view( TBSC_VIEWS_DIR . 'archive/entry.php' ) );
+	}
 }
